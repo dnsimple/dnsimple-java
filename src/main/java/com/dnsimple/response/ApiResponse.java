0989341e310a39604f5548ae5227406f1ede43e9
Guille <ggalmazor@gmail.com>
2020-06-29 17:59:48 +0200

    Remove the HTTP request and response from our ApiResponse objects
    This couples our most abstract representation of API responses with an implementation library that we want to abstract away from.

--- src/main/java/com/dnsimple/response/ApiResponse.java
+++ src/main/java/com/dnsimple/response/ApiResponse.java
@@ -1,25 +1,4 @@
 package com.dnsimple.response;
 
-import com.google.api.client.http.HttpRequest;
-import com.google.api.client.http.HttpResponse;
-
 public abstract class ApiResponse {
-  private HttpRequest httpRequest;
-  private HttpResponse httpResponse;
-
-  public HttpRequest getHttpRequest() {
-    return httpRequest;
-  }
-
-  public void setHttpRequest(HttpRequest httpRequest) {
-    this.httpRequest = httpRequest;
-  }
-
-  public HttpResponse getHttpResponse() {
-    return httpResponse;
-  }
-
-  public void setHttpResponse(HttpResponse httpResponse) {
-    this.httpResponse = httpResponse;
-  }
 }
