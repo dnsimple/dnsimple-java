4dd2359e22a148e53279e394291e92f84173694b
Guille <ggalmazor@gmail.com>
2020-06-29 18:10:12 +0200

    Extract an HttpEndpointClient interface and a Google API HTTP client implementation

---
+++ src/main/java/com/dnsimple/endpoints/http/GoogleHttpEndpointClient.java
@@ -0,0 +1,220 @@
+package com.dnsimple.endpoints.http;
+
+import com.dnsimple.Dnsimple;
+import com.dnsimple.exception.DnsimpleException;
+import com.dnsimple.request.Filter;
+import com.dnsimple.response.ApiResponse;
+import com.google.api.client.http.GenericUrl;
+import com.google.api.client.http.HttpContent;
+import com.google.api.client.http.HttpHeaders;
+import com.google.api.client.http.HttpMethods;
+import com.google.api.client.http.HttpRequest;
+import com.google.api.client.http.HttpResponse;
+import com.google.api.client.http.HttpResponseException;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.apache.v2.ApacheHttpTransport;
+import com.google.api.client.http.json.JsonHttpContent;
+import com.google.api.client.json.JsonParser;
+import com.google.api.client.json.gson.GsonFactory;
+import io.mikael.urlbuilder.UrlBuilder;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Map;
+
+public class GoogleHttpEndpointClient implements HttpEndpointClient {
+
+  public static final String DEFAULT_USER_AGENT = "dnsimple-java/" + Dnsimple.VERSION;
+
+  private static final String API_VERSION_PATH = "/v2/";
+  private static final String MIME_APPLICATION_JSON = "application/json";
+
+  private HttpTransport transport;
+  private String accessToken;
+  private String userAgent;
+
+  public GoogleHttpEndpointClient() {
+    this.transport = new ApacheHttpTransport();
+  }
+
+  /**
+   * Set the underlying transport mechanism.
+   * <p>
+   * This method is primarily used to specify a mocked transport layer during testing.
+   *
+   * @param transport The transport instance
+   */
+  public void setTransport(HttpTransport transport) {
+    this.transport = transport;
+  }
+
+  /**
+   * Set the access token to use for the client instance.
+   *
+   * @param accessToken The access token string
+   */
+  public void setAccessToken(String accessToken) {
+    this.accessToken = accessToken;
+  }
+
+  /**
+   * Set the user agent to use for the client instance.
+   *
+   * @param userAgent The user agent string
+   */
+  public void setUserAgent(String userAgent) {
+    this.userAgent = userAgent;
+  }
+
+
+  @Override
+  public HttpResponse get(String path) throws DnsimpleException, IOException {
+    return get(path, null);
+  }
+
+  @Override
+  public HttpResponse get(String path, Map<String, Object> options) throws DnsimpleException, IOException {
+    return request(HttpMethods.GET, versionedPath(path), null, options);
+  }
+
+  @Override
+  public HttpResponse post(String path) throws DnsimpleException, IOException {
+    return post(path, null);
+  }
+
+  @Override
+  public HttpResponse post(String path, Map<String, Object> attributes) throws DnsimpleException, IOException {
+    return post(path, attributes, null);
+  }
+
+  @Override
+  public HttpResponse post(String path, Map<String, Object> attributes, Map<String, Object> options) throws DnsimpleException, IOException {
+    return request(HttpMethods.POST, versionedPath(path), attributes, null);
+  }
+
+  @Override
+  public HttpResponse put(String path) throws DnsimpleException, IOException {
+    return put(path, null);
+  }
+
+  @Override
+  public HttpResponse put(String path, Object attributes) throws DnsimpleException, IOException {
+    return put(path, attributes, null);
+  }
+
+  @Override
+  public HttpResponse put(String path, Object attributes, Map<String, Object> options) throws DnsimpleException, IOException {
+    return request(HttpMethods.PUT, versionedPath(path), attributes, null);
+  }
+
+  @Override
+  public HttpResponse patch(String path, Object attributes) throws DnsimpleException, IOException {
+    return patch(path, attributes, null);
+  }
+
+  @Override
+  public HttpResponse patch(String path, Object attributes, Map<String, Object> options) throws DnsimpleException, IOException {
+    return request(HttpMethods.PATCH, versionedPath(path), attributes, null);
+  }
+
+  @Override
+  public HttpResponse delete(String path) throws DnsimpleException, IOException {
+    return delete(path, null);
+  }
+
+  @Override
+  public HttpResponse delete(String path, Map<String, Object> options) throws DnsimpleException, IOException {
+    return request(HttpMethods.DELETE, versionedPath(path), null, null);
+  }
+
+
+  private HttpResponse request(String method, String url, Object data, Map<String, Object> options) throws DnsimpleException, IOException {
+    HttpContent content = null;
+    if (data != null) {
+      content = new JsonHttpContent(new GsonFactory(), data);
+    }
+
+    HttpRequest request = transport.createRequestFactory().buildRequest(method, buildUrl(url, options), content);
+
+    HttpHeaders headers = request.getHeaders();
+    headers.setAccept(MIME_APPLICATION_JSON);
+
+    // Add the user agent string to the headers
+    ArrayList<String> fullUserAgent = new ArrayList<String>();
+    if (userAgent != null) {
+      fullUserAgent.add(userAgent);
+    }
+    fullUserAgent.add(DEFAULT_USER_AGENT);
+    headers.setUserAgent(String.join(" ", fullUserAgent));
+
+    // Add the authorization to the headers
+    headers.setAuthorization("Bearer " + accessToken);
+
+    if (data != null) {
+      headers.setContentType(MIME_APPLICATION_JSON);
+    }
+
+    try {
+      return request.execute();
+    } catch (HttpResponseException e) {
+      throw DnsimpleException.transformException(e);
+    }
+  }
+
+  /**
+   * Parse the response from the HTTP call into an instance of the given class.
+   *
+   * @param response The parsed response object
+   * @param c        The class to instantiate and use to build the response object
+   * @return The ApiResponse object
+   * @throws IOException Any IO errors
+   */
+  public ApiResponse parseResponse(HttpResponse response, Class<?> c) throws IOException {
+    return (ApiResponse) parse(response, c);
+  }
+
+  public Object parse(HttpResponse response, Class<?> c) throws IOException {
+    if (response.getStatusCode() == 204 || response.getContent() == null)
+      return buildTypeSafe(c);
+
+    try (InputStream in = response.getContent()) {
+      JsonParser jsonParser = GsonFactory.getDefaultInstance().createJsonParser(in);
+      return jsonParser.parse(c);
+    }
+  }
+
+  @SuppressWarnings("deprecation")
+  private ApiResponse buildTypeSafe(Class<?> c) {
+    try {
+      return (ApiResponse) c.newInstance();
+    } catch (ReflectiveOperationException e) {
+      throw new RuntimeException("Cannot instantiate " + c, e);
+    }
+  }
+
+  private String versionedPath(String path) {
+    return Dnsimple.getApiBase() + API_VERSION_PATH + path;
+  }
+
+  private GenericUrl buildUrl(String url, Map<String, Object> options) {
+    if (options == null) {
+      options = Collections.emptyMap();
+    }
+
+    UrlBuilder urlBuilder = UrlBuilder.fromString(url);
+    if (options.containsKey("filter")) {
+      Filter filter = (Filter) options.remove("filter");
+      urlBuilder = urlBuilder.addParameter(filter.name, filter.value);
+    }
+
+    if (options.size() > 0) {
+      for (Map.Entry<String, Object> kv : options.entrySet()) {
+        urlBuilder = urlBuilder.addParameter(kv.getKey(), kv.getValue().toString());
+      }
+    }
+
+    return new GenericUrl(urlBuilder.toUrl());
+  }
+
+}
--- src/test/java/com/dnsimple/ClientTest.java
+++ src/test/java/com/dnsimple/ClientTest.java
@@ -1,22 +1,20 @@
 package com.dnsimple;
 
-import static com.dnsimple.tools.HttpMethod.*;
+import static com.dnsimple.tools.HttpMethod.GET;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.Matchers.is;
 
 import com.dnsimple.exception.DnsimpleException;
-import com.dnsimple.tools.HttpMethod;
 import java.io.IOException;
 import org.junit.Test;
 
 public class ClientTest extends DnsimpleTestBase {
-  private static final String TEST_ACCESS_TOKEN = "test-access-token";
+
 
   @Test
   public void testAuthorizationHeader() throws DnsimpleException, IOException {
     server.stubFixtureAt("listAccounts/success-account.http");
-    client.setAccessToken(TEST_ACCESS_TOKEN);
 
     client.accounts.listAccounts();
     assertThat(server.getRecordedRequest().getMethod(), is(GET));
@@ -27,11 +25,10 @@ public class ClientTest extends DnsimpleTestBase {
   @Test
   public void testUserAgentHeader() throws DnsimpleException, IOException {
     server.stubFixtureAt("listAccounts/success-account.http");
-    client.setUserAgent("my-user-agent");
 
     client.accounts.listAccounts();
     assertThat(server.getRecordedRequest().getMethod(), is(GET));
     assertThat(server.getRecordedRequest().getPath(), is("/v2/accounts"));
-    assertThat(server.getRecordedRequest().getHeaders(), hasEntry("User-Agent", "my-user-agent dnsimple-java/" + Dnsimple.VERSION + " Google-HTTP-Java-Client/1.35.0 (gzip)"));
+    assertThat(server.getRecordedRequest().getHeaders(), hasEntry("User-Agent", TEST_USER_AGENT + " dnsimple-java/" + Dnsimple.VERSION + " Google-HTTP-Java-Client/1.35.0 (gzip)"));
   }
 }
