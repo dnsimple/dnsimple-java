a85bbd119a9c3d75a7988c14dd526289d3f6d9be
Guille <ggalmazor@gmail.com>
2020-07-05 12:10:57 +0200

    Remove the last bits of the library that were coupled to a specific HTTP client implementation detail

--- src/main/java/com/dnsimple/response/ApiResponse.java
+++ src/main/java/com/dnsimple/response/ApiResponse.java
@@ -1,25 +1,5 @@
 package com.dnsimple.response;
 
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 public abstract class ApiResponse {
-    private HttpRequest httpRequest;
-    private HttpResponse httpResponse;
-
-    public HttpRequest getHttpRequest() {
-        return httpRequest;
-    }
-
-    public void setHttpRequest(HttpRequest httpRequest) {
-        this.httpRequest = httpRequest;
-    }
-
-    public HttpResponse getHttpResponse() {
-        return httpResponse;
-    }
-
-    public void setHttpResponse(HttpResponse httpResponse) {
-        this.httpResponse = httpResponse;
-    }
 }
