678567ce03d70d7de5f240c6b42435b757daf81d
Guille <ggalmazor@gmail.com>
2020-07-06 17:15:36 +0200

    Remove unused methods of the exception types

--- src/main/java/com/dnsimple/exception/BadRequestException.java
+++ src/main/java/com/dnsimple/exception/BadRequestException.java
@@ -8,11 +8,6 @@ public class BadRequestException extends DnsimpleException {
         this.statusCode = statusCode;
     }
 
-    public BadRequestException(int statusCode, Throwable cause) {
-        super("The server responded with an HTTP " + statusCode + " error. Please, review the request options and attributes", cause);
-        this.statusCode = statusCode;
-    }
-
     public int getStatusCode() {
         return statusCode;
     }
