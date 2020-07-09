0a3e068ba08126a76443e0e4cf6c8963f5d3d160
Guillermo Gutiérrez <ggalmazor@gmail.com>
2020-07-09 12:11:40 +0200

    Remove the unused global state defined at Dnsimple

--- src/main/java/com/dnsimple/Dnsimple.java
+++ src/main/java/com/dnsimple/Dnsimple.java
@@ -2,15 +2,4 @@ package com.dnsimple;
 
 public abstract class Dnsimple {
     public static final String VERSION = "0.7.1";
-    public static final String PRODUCTION_API_BASE = "https://api.dnsimple.com";
-    private static volatile String apiBase = PRODUCTION_API_BASE;
-
-    public static String setApiBase(String overriddenApiBase) {
-        apiBase = overriddenApiBase;
-        return apiBase;
-    }
-
-    public static String getApiBase() {
-        return apiBase;
-    }
 }
