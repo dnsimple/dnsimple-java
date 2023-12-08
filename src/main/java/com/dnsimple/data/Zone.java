b6b1e8506674f0d99658311fd0864efc68a395d9
Stephen Nelson <stephen.nelson@dnsimple.com>
2023-12-08 13:31:14 -0500

    add missing fields to the zone struct

--- src/main/java/com/dnsimple/data/Zone.java
+++ src/main/java/com/dnsimple/data/Zone.java
@@ -7,14 +7,20 @@ public class Zone {
     private final Long accountId;
     private final String name;
     private final Boolean reverse;
+    private final Boolean secondary;
+    private final OffsetDateTime last_transferred_at;
+    private final Boolean active;
     private final OffsetDateTime createdAt;
     private final OffsetDateTime updatedAt;
 
-    public Zone(Long id, Long accountId, String name, Boolean reverse, OffsetDateTime createdAt, OffsetDateTime updatedAt) {
+    public Zone(Long id, Long accountId, String name, Boolean reverse, Boolean secondary, OffsetDateTime last_transferred_at, Boolean active, OffsetDateTime createdAt, OffsetDateTime updatedAt) {
         this.id = id;
         this.accountId = accountId;
         this.name = name;
         this.reverse = reverse;
+        this.secondary = secondary;
+        this.last_transferred_at = last_transferred_at;
+        this.active = active;
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
@@ -33,6 +39,18 @@ public class Zone {
 
     public Boolean isReverse() {
         return reverse;
+    }
+
+    public Boolean isSecondary() {
+        return secondary;
+    }
+
+    public OffsetDateTime getLastTransferredAt() {
+        return last_transferred_at;
+    }
+
+    public Boolean isActive() {
+        return active;
     }
 
     public OffsetDateTime getCreatedAt() {
