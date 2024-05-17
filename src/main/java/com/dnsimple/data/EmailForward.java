56de6ec2579e7ca236f62b99cb5116fe11af236e
Guillermo Gutierrez Almazor <ggalmazor@gmail.com>
2024-05-17 10:14:28 +0200

    Change: remove unused private fields

--- src/main/java/com/dnsimple/data/EmailForward.java
+++ src/main/java/com/dnsimple/data/EmailForward.java
@@ -5,18 +5,14 @@ import java.time.OffsetDateTime;
 public class EmailForward {
     private final Long id;
     private final Long domainId;
-    private final String from;
-    private final String to;
     private final String aliasEmail;
     private final String destinationEmail;
     private final OffsetDateTime createdAt;
     private final OffsetDateTime updatedAt;
 
-    public EmailForward(Long id, Long domainId, String from, String to, String aliasEmail, String destinationEmail, OffsetDateTime createdAt, OffsetDateTime updatedAt) {
+    public EmailForward(Long id, Long domainId, String aliasEmail, String destinationEmail, OffsetDateTime createdAt, OffsetDateTime updatedAt) {
         this.id = id;
         this.domainId = domainId;
-        this.from = from;
-        this.to = to;
         this.aliasEmail = aliasEmail;
         this.destinationEmail = destinationEmail;
         this.createdAt = createdAt;
